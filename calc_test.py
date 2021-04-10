06e5ccb9d0f2df7e34dc81d765f1e6ccb3fa8333
akr13 <akregmi@gmail.com>
2021-04-10 14:32:21 -0400

    added .travis.yml

--- calc_test.py
+++ calc_test.py
@@ -1,8 +1,22 @@
 import unittest
+from calculator import Calculator
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)
+    def test_addition(self):
+        instance = Calculator(6, 3)
+        self.assertEqual(instance.add(), 9)
+    def test_subtraction(self):
+        instance = Calculator(6, 3)
+        self.assertEqual(instance.sub(), 3)
+    def test_division(self):
+        instance = Calculator(6, 3)
+        self.assertEqual(instance.divide(), 2)
+    def test_multiplication(self):
+        instance = Calculator(6, 3)
+        self.assertEqual(instance.multiply(), 18)
+
 
 if __name__ == '__main__':
     unittest.main()
